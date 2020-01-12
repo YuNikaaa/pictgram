83bd44c64556f6f1f51da65ef43283b50329d249
”YU” <spasiba123456789@yahoo.co.jp>
2020-01-12 17:11:39 +0000

    kadai

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+has_secure_password
+  validates :password, length: { minimum: 8,maximum: 32 }
+VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
+　validates :password, format: { with: VALID_PASSWORD_REGEX }
+VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
+  validates :email, format: { with: VALID_PASSWORD_EMAIL }
+　validates :name, length: { minimum: 1, maximum: 10 }
+end
