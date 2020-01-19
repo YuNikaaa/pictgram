8dd5743c0526c524c6cbe236f373c8dd96c3b5f8
”YU” <spasiba123456789@yahoo.co.jp>
2020-01-19 14:06:06 +0000

    kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
 　validates :password, format: { with: VALID_PASSWORD_REGEX }
 VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
   validates :email, format: { with: VALID_PASSWORD_EMAIL }
-　validates :name, length: { minimum: 1, maximum: 10 }
+　validates :name, length: { minimum: 1, maximum: 15 }
 end
