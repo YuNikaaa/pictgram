892081012d113137e0554c6e288559cba62a482b
”YU” <spasiba123456789@yahoo.co.jp>
2020-02-24 17:33:40 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,16 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+end
